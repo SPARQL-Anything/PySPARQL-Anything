a882fad528b57fe0d82854166c148ed005227077
Marco <marco1791@protonmail.com>
2023-01-10 17:54:58 +0000

    New exception handling in object construction. v1.0 of the unit testing script

--- src/pysparql_anything.py
+++ src/pysparql_anything.py
@@ -1,16 +1,13 @@
 # The class PySpyrqlAnything provides a Python CLI for the SPARQL Anything
-# technology.
-# It replaces the regular command line instructions with a call to the run()
-# method.
-# It also provides methods to handle ASK, CONSTRUCT and SELECT queries and
-# return their results as Python objects.
+# technology. It replaces the regular command line instructions
+# with a call to the run() method.
 #
 # @author Marco Ratta & Enrico Daga
-# @version 25/11/2022 v1.5
+# @version 10/01/2023 v1.6
 
+from rdflib import Graph
 import json
 import jnius_config
-from rdflib import Graph
 
 class PySparqlAnything:
 
@@ -30,14 +27,17 @@ class PySparqlAnything:
             # JVM configuration
             jnius_config.set_classpath(aPath)
             # Launch JVM
-            from jnius import autoclass
+            from jnius import autoclass, JavaException
             return autoclass('com.github.sparqlanything.cli.SPARQLAnything')
-        except: # Handles JVM exception
+        except ValueError as e:
+            print('Cannot construct two objects for the same VM. \n'
+                  + 'Please create a new VM for a new engine \n' )
+            print(e)
+        except JavaException as e:
+            # Handles JVM exception for an incorrect path
             print('JVM exception occured: check the path passed as input.\n'
-                  + 'CLI will be restarted.')
-            from jnius import detach
-            detach()
-            os.system("python")
+                  + 'CLI must be restarted. \n')
+            print(e)
 
     # The run method replaces the regular command line execution.
     # @param **kwargs The keyword arguments are the same as the regular
@@ -50,6 +50,16 @@ class PySparqlAnything:
             return
         else:
             self.reflection.main(args)
+            
+    # The select method enables one to run a SELECT query and return the result
+    # as a Python dictionary.
+    # @param q The path to the query.
+    # @return a Python dictionary
+    
+    def select(self, **kwargs):
+        kwargs['f'] = 'json'
+        args = buildArgs(kwargs)
+        return json.loads(self.reflection.callMain(args))
 
     # The ask method enables one to run an ASK query and return the result as
     # a Python boolean True or False.
@@ -64,7 +74,7 @@ class PySparqlAnything:
             return True
         else:
             return False
-
+        
     # The construct method enables one to run a CONSTRUCT query and return the
     # result as a rdflib graph object.
     # @param q The path to the query
@@ -75,16 +85,7 @@ class PySparqlAnything:
         string = self.reflection.callMain(args)
         g = Graph().parse(data = string)
         return g
-
-    # The select method enables one to run a SELECT query and return the result
-    # as a Python dictionary.
-    # @param q The path to the query.
-    # @return a Python dictionary
-    
-    def select(self, **kwargs):
-        kwargs['f'] = 'json'
-        args = buildArgs(kwargs)
-        return json.loads(self.reflection.callMain(args))
+        
         
 # Helper for the run method. Constructs the appropriate String array
 # to pass to the main method from Python **kwargs.
---
+++ test/unitTest.py
@@ -0,0 +1,44 @@
+# Unit testing script for the PySPARQL Anything module.
+# For the construction of this script, files from the book 'Learning SPARQL'
+# by Bob DuCharme have been used.
+#
+# @author Marco Ratta
+# @version 10/01/2021 v1.0
+
+import unittest
+import pysparql_anything as spy
+from rdflib import Graph
+
+# Local path to SPARQL anything jar.
+jar = 'C:/Users/Marco/GitProjects/sparql.anything/sparql-anything-cli/target/sparql-anything-0.9.0-SNAPSHOT.jar'
+
+# Test case for functionalities.
+class PySparqlTestCase(unittest.TestCase):
+
+    def test_ask(self):
+        self.assertTrue(engine.ask(q='queries/ask/ex199.rq', l='ex198.ttl'))
+        self.assertTrue(engine.ask(q='queries/ask/ex202.rq', l='ex198.ttl'))
+        self.assertFalse(engine.ask(q='queries/ask/ex199.rq', l='ex012.ttl'))
+        self.assertFalse(engine.ask(q='queries/ask/ex199.rq', l='ex012.ttl'))
+
+    def test_construct(self):
+        g_1 = Graph().parse('ex189.txt', format='ttl')
+        x_1 = engine.construct(q='queries/construct/ex188.rq', l='ex187.ttl')
+        g_2 = Graph().parse('ex191.txt', format='ttl')
+        x_2 = engine.construct(q='queries/construct/ex190.rq', l='ex187.ttl')
+        self.assertEqual(len(x_1 - g_1), 0)
+        self.assertEqual(len(x_2 - g_2), 0)
+
+    def test_select(self):
+        d_1 = {'head': {'vars': ['seriesName']},
+               'results': {'bindings': [{'seriesName': {'type': 'literal', 'value': 'Cougar Town'}}, {'seriesName': {'type': 'literal', 'value': 'Friends'}}]}
+        }
+        d_2 = {'head': {'vars': ['first', 'last', 'workTel']},
+               'results': {'bindings': [{'first': {'type': 'literal', 'value': 'Craig'}, 'last': {'type': 'literal', 'value': 'Ellis'}, 'workTel': {'type': 'literal', 'value': '(245) 315-5486'}}]}
+        }
+        self.assertEqual(engine.select(q='queries/select/testSelect.sparql'), d_1)
+        self.assertEqual(engine.select(q='queries/select/ex055.rq', l='ex054.ttl'), d_2)
+
+if __name__ == '__main__':
+    engine = spy.PySparqlAnything(jar)
+    unittest.main()
