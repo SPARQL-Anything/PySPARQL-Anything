6d7c5ef616e1160a67593e50dc26bc3ce8644d2a
MarcoR1791 <marco1791@protonmail.com>
2024-03-01 00:42:54 +0000

    __about__.py fix

--- src/pysparql_anything/__about__.py
+++ src/pysparql_anything/__about__.py
@@ -9,12 +9,12 @@ Date: 29/02/2024
 
 # PySPARQL ANYTHING METADATA
 # PySPARQL version for the build process:
-__version__ = '0.9.0.2'
+__version__ = "0.9.0.2"
 
 # SPARQL ANYTHING METADATA
 # Version of SPARQL Anything to download:
-__SparqlAnything__ = '0.9.0'
+__SparqlAnything__ = "0.9.0"
 # Path to the SPARQL Anything main class within the executable jar:
-__jarMainPath__ = 'io.github.sparqlanything.cli.SPARQLAnything'
+__jarMainPath__ = "io.github.sparqlanything.cli.SPARQLAnything"
 # SPARQL Anything GitHub URI:
-__uri__ = 'SPARQL-Anything/sparql.anything'
+__uri__ = "SPARQL-Anything/sparql.anything"
